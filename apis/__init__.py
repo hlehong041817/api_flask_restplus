2c3a2208c3a4eacc8ffc2d1f423ab9513ac5b820
hlehong041817 <ha.lehong@hotmail.com>
2018-03-31 19:45:53 +0200

    swagger

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,12 +2,14 @@ from flask_restplus import Api
 
 from .cat import api as cat_api
 from .dog import api as dog_api
+from .xumii import api as xummi_api
 
 api = Api(
-    title='Zoo API',
+    title='XuMii',
     version='1.0',
-    description='A simple demo API',
+    description='X User Matter into Information',
 )
 
-api.add_namespace(cat_api)
-api.add_namespace(dog_api)
+# api.add_namespace(cat_api)
+# api.add_namespace(dog_api)
+api.add_namespace(xummi_api)
---
+++ apis/xumii.py
@@ -0,0 +1,36 @@
+from flask_restplus import Namespace, Resource, fields
+
+api = Namespace('xumii', description='X User Matter into Information')
+
+message_item = api.model('Message', {
+    'text': fields.String(required=True, description='The message_item identifier'),
+    'from': fields.String(required=True, description='The message_item name'),
+})
+
+MessageDict = [
+    {'text': 'Hey', 'from': 'guest'},
+    {'text': 'Hi', 'from': 'xummi'},
+]
+
+
+@api.route('/')
+class MessageList(Resource):
+    @api.doc('list_cats')
+    @api.marshal_list_with(message_item)
+    def get(self):
+        '''List all cats'''
+        return MessageDict
+
+
+@api.route('/<id>')
+@api.param('id', 'The message_item identifier')
+@api.response(404, 'MessageItem not found')
+class MessageItem(Resource):
+    @api.doc('get_cat')
+    @api.marshal_with(message_item)
+    def get(self, id):
+        '''Fetch a message_item given its identifier'''
+        for message_item in MessageDict:
+            if message_item['text'] == id:
+                return message_item
+        api.abort(404)
